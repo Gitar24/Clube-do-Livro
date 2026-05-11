03b4da86fd648ef7716ca325fd7f1410f26f5b5b
Gitar24 <arthurlopespassos159@gmail.com>
2026-05-11 16:04:08 -0300

    consertando bugs

---
+++ seed.sql
@@ -0,0 +1,5 @@
+insert into livros(titulo,autor,genero,ano_publicacao)
+values 
+('O Hobbit','J.R.R. Tolkien','Fantasia','09-21-1937'),
+('O Cortiço','Aluísio Azevedo','Naturalismo','01-01-1890'),
+('Memórias Postumas de Brás Cubas','Machado de Assis','Realismo','01-01-1881')
